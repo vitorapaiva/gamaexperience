2d9e4226691c8be46e4c6dc5c6889f7979dd08df
Vitor Paiva <vitorapaiva@gmail.com>
2017-05-21 11:50:56 -0300

    Adicao do Google Analytics

--- blog/resources/views/index.blade.php
+++ blog/resources/views/index.blade.php
@@ -8,6 +8,16 @@
     <link rel="stylesheet" href="css/app.css">
     <link href="https://fonts.googleapis.com/css?family=Dosis:100,300,400,600,700%7COpen+Sans:400,500,600" rel="stylesheet" type="text/css">
     <script src="https://use.fontawesome.com/c66c33c7d6.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-99615858-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>   
 </head>
 
 <body ng-app="MyInvest">
