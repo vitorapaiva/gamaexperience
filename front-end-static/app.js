cc4b13a7f9d24bfe3ecbaab7fc5e56ef6343cc58
Shu Min Yang Chang <shu.minyang@hotmail.com>
2017-05-25 22:12:46 -0300

    commit front end novo layout

--- front-end-static/app.js
+++ front-end-static/app.js
@@ -1,26 +1,30 @@
-$(function() {
+$(function () {
     'use strict';
-    
-    $('#email').keyup(function(event) {
+
+    $('#email').keyup(function (event) {
         var email = this.value;
 
         if (!validarEmail(email)) {
-            $(this).addClass('input-erro');            
+            $(this).addClass('input-erro');
         } else {
             $(this).removeClass('input-erro');
         }
-        
+
     });
+
+    var scrollTop = $(window).scrollTop();
+    var fixTop = $('#fixTop').offset().top;
+    var distance = fixTop - scrollTop;    
 
     $('#formFix').affix({
         offset: {
-            top: 150,
+            top: distance,
             bottom: $('#bottom-help').outerHeight(true)
         }
-    });    
+    });
 });
 
- var validarEmail = function (email) {
+var validarEmail = function (email) {
     var atpos = email.indexOf('@');
     var dotpos = email.lastIndexOf('.');
     if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= email.length) {
